3b0b172507367c4020666731a877e5c2a07a6209
AlexPrn <alexpournaras95@gmail.com>
2025-03-29 09:51:16 +0200

    More tests on linked_list.c made and passed. Next commit: More (maybe all) tests for linked_list.c

--- test/test_linked_list.c
+++ test/test_linked_list.c
@@ -9,12 +9,18 @@
 
 
 void test_init_linked_list();
+void test_is_empty();
+void test_add_at_head();
+void test_add_at_tail();
 
 void add_linked_list_tests();
 
 void add_linked_list_tests(){
   CU_pSuite suite = CU_add_suite("linked_list", 0, 0);
   CU_add_test(suite, "test_init_linked_list", test_init_linked_list);
+  CU_add_test(suite, "test_is_empty", test_is_empty);
+  CU_add_test(suite, "test_add_at_head", test_add_at_head);
+  CU_add_test(suite, "test_add_at_tail", test_add_at_tail);
 }
 
 
@@ -27,4 +33,46 @@ void test_init_linked_list(){
   free(list);
 }
 
+void test_is_empty() {
+  LinkedList* list = init_linked_list();
+  CU_ASSERT_TRUE(is_empty(list));
+
+  Node* node = get_new_node();
+  add_at_head(list, node);
+  CU_ASSERT_FALSE(is_empty(list));
+
+  free_linked_list(list);
+}
+
+void test_add_at_head() {
+  LinkedList* list = init_linked_list();
+  Node* node = get_new_node();
+
+  add_at_head(list, node);
+  CU_ASSERT_PTR_EQUAL(list->head, node);
+  CU_ASSERT_PTR_EQUAL(list->tail, node);
+  CU_ASSERT_PTR_NULL(list->head->next);
+
+  Node* node2 = get_new_node();
+  add_at_head(list, node2);
+  CU_ASSERT_PTR_EQUAL(list->head, node2);
+  CU_ASSERT_PTR_EQUAL(list->head->next, node);
+
+  free_linked_list(list);
+}
+
+void test_add_at_tail() {
+  LinkedList* list = init_linked_list();
+  Node* node = get_new_node();
+  add_at_tail(list, node);
+  CU_ASSERT_PTR_EQUAL(list->tail, node);
+  CU_ASSERT_PTR_EQUAL(list->head, node);
+  CU_ASSERT_PTR_NULL(list->tail->next);
+
+  Node* node2 = get_new_node();
+  add_at_tail(list, node2);
+  CU_ASSERT_PTR_EQUAL(list->tail, node2);
+  CU_ASSERT_PTR_EQUAL(list->head->next, node2);
+
+}
 
--- test/test_runner.c
+++ test/test_runner.c
@@ -8,12 +8,15 @@
 
 // Declare the functions that add the test suites
 void add_node_tests();
+void add_linked_list_tests();
+
 
 int main() {
     CU_initialize_registry();
 
     // Add all test suites
     add_node_tests();
+    add_linked_list_tests();
 
     // Set the CUnit mode
     CU_basic_set_mode(CU_BRM_VERBOSE);
