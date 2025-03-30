41b4e6532fd6609a12b4141a0f355bf211d2ee92
AlexPrn <alexpournaras95@gmail.com>
2025-03-30 15:28:14 +0300

    Updated project's Recuirements (reports pdf)

--- cmake/FindCUnit.cmake
+++ cmake/FindCUnit.cmake
@@ -3,12 +3,22 @@
 
 find_path(CUNIT_INCLUDE_DIR
         NAMES CUnit/CUnit.h
-        PATHS /usr/include /usr/local/include /opt/homebrew/include
+        PATHS
+        $ENV{CUNIT_ROOT}/include  # Allow users to specify CUNIT_ROOT
+        ${CMAKE_PREFIX_PATH}/include
+        /usr/include
+        /usr/local/include
+        /opt/homebrew/include
 )
 
 find_library(CUNIT_LIBRARY
         NAMES cunit
-        PATHS /usr/lib /usr/local/lib /opt/homebrew/lib
+        PATHS
+        $ENV{CUNIT_ROOT}/lib
+        ${CMAKE_PREFIX_PATH}/lib
+        /usr/lib
+        /usr/local/lib
+        /opt/homebrew/lib
 )
 
 include(FindPackageHandleStandardArgs)
@@ -17,9 +27,11 @@ find_package_handle_standard_args(CUnit DEFAULT_MSG CUNIT_INCLUDE_DIR CUNIT_LIBR
 if(CUNIT_FOUND)
     set(CUNIT_LIBRARIES ${CUNIT_LIBRARY})
     set(CUNIT_INCLUDE_DIRS ${CUNIT_INCLUDE_DIR})
+    message(STATUS "Found CUnit: ${CUNIT_LIBRARY}")
 else()
     set(CUNIT_LIBRARIES "")
     set(CUNIT_INCLUDE_DIRS "")
+    message(WARNING "CUnit not found. Tests may not compile.")
 endif()
 
 mark_as_advanced(CUNIT_INCLUDE_DIR CUNIT_LIBRARY)
--- src/main.c
+++ src/main.c
@@ -6,20 +6,6 @@
 
 
 int main(void) {
-    // LinkedList* list = init_linked_list();
-    //
-    // for (int i = 0; i < 13; i++) {
-    //     add_at_tail(list, get_new_node());
-    // }
-    //
-    // print_linked_list(list);
-    // delete_at_head(list);
-    // print_linked_list(list);
-    // add_empty_node_at_head(list);
-    // add_empty_node_at_head(list);
-    // print_linked_list(list);
-    //
-    // free_linked_list(list);
-
+    // Feel free to play
     return 0;
 }
